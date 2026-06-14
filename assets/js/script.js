44a9fccfaa2aa3ed454f4dbd50ac9f4d6a6726b8
Anne-Cécile Mainier <annececile.mainier@gmail.com>
2026-06-14 13:14:11 +0100

    feat: edit and save existing tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,15 +17,47 @@ function onClickBtn() {
     newTaskEl.classList.add("task-item");
 
     const newTaskHTML = `<span class="task-text">${cleanTaskInput}</span>
-    <button class="delete-btn">Delete</button>`;
+    <button class="delete-btn">Delete</button><button class="edit-btn">Edit</button>`;
 
     newTaskEl.innerHTML = newTaskHTML;
 
+    // Task deletion
     const deleteBtn = newTaskEl.querySelector(".delete-btn");
     deleteBtn.addEventListener("click", onClickDelete);
 
     function onClickDelete() {
       newTaskEl.remove();
+    }
+
+    // Task edition
+    const editBtn = newTaskEl.querySelector(".edit-btn");
+    editBtn.addEventListener("click", onClickEdit);
+
+    function onClickEdit() {
+      const currentTaskTextEl = newTaskEl.querySelector(".task-text");
+      if (editBtn.textContent === "Edit") {
+        const currentTaskText = currentTaskTextEl.textContent;
+        const editInputEl = document.createElement("input");
+        editInputEl.classList.add("edit-input");
+        editInputEl.value = currentTaskText;
+
+        currentTaskTextEl.insertAdjacentElement("afterend", editInputEl);
+        currentTaskTextEl.style.display = "none";
+        editBtn.textContent = "Save";
+      } else {
+        const editTaskTextEl = newTaskEl.querySelector(".edit-input");
+        const editTaskText = editTaskTextEl.value.trim();
+        if (editTaskText === "") {
+          errorMessageEl.textContent = "That's an empty task, browski!";
+          errorMessageEl.classList.remove("hidden");
+          return;
+        }
+        errorMessageEl.classList.add("hidden");
+        currentTaskTextEl.textContent = editTaskText;
+        editTaskTextEl.remove();
+        currentTaskTextEl.style.display = "";
+        editBtn.textContent = "Edit";
+      }
     }
 
     taskListEl.appendChild(newTaskEl);
